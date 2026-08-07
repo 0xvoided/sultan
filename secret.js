3806f7b3a89a0b091ad72ba6978262cac2d7ab2b
SyntaxError <throwvoid1@outlook.com>
2026-08-07 03:16:39 +0300

    Add files via upload

---
+++ secret.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  songTitle:  "Van Gogh",
+  songArtist: "Virginio Aiello",
+  discordId: "374351552720142336",
+  socials: {
+    instagram: "https://instagram.com/throwvoid",
+    twitter:   "https://x.com/sultanwqr",
+    tiktok:    "https://tiktok.com/@0xvoidedd"
+  }
+};
