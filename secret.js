535dcbbb4b1a4da3491bfb4666b33575e253aad2
SyntaxError <throwvoid1@outlook.com>
2026-09-01 22:15:07 +0300

    Add files via upload

--- secret.js
+++ secret.js
@@ -1,7 +1,12 @@
 const CONFIG = {
-  songTitle:  " No One Noticed",
-  songArtist: "'The Marías",
+  songTitle:  "We never dated",
+  songArtist: "'Sombr",
+  songArt: "https://i.pinimg.com/736x/e6/4f/cb/e64fcbcbb1d1332475097ce908111210.jpg",
   discordId: "374351552720142336",
+  badges: {
+    nitro: false,
+    boost: false
+  },
   socials: {
     instagram: "https://instagram.com/throwvoid",
     twitter:   "https://x.com/sultanwqr",
