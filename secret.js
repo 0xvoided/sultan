f647bc17ef5cc9410751dbc2985d4c5329453d54
SyntaxError <throwvoid1@outlook.com>
2026-09-07 03:31:43 +0300

    Add files via upload

---
+++ secret.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+  songTitle:  "We never dated",
+  songArtist: "'Sombr",
+  songArt: "https://i.pinimg.com/736x/e6/4f/cb/e64fcbcbb1d1332475097ce908111210.jpg",
+  discordId: "374351552720142336",
+  socialUrl: "https://discord.com/users/374351552720142336",
+  badges: {
+    nitro: false,
+    boost: false
+  },
+  socials: {
+    instagram: "https://instagram.com/throwvoid",
+    twitter:   "https://x.com/sultanwqr",
+    tiktok:    "https://tiktok.com/@0xvoidedd"
+  }
+};
