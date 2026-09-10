23ff05475d2416c6f1ef3144d2eaebe882944543
SyntaxError <throwvoid1@outlook.com>
2026-09-10 15:55:20 +0300

    Add files via upload

--- secret.js
+++ secret.js
@@ -1,16 +1,12 @@
 const CONFIG = {
-  songTitle:  "Spiracle",
-  songArtist: "Flower Face",
-  songArt: "https://i.pinimg.com/1200x/58/47/0c/58470c5384c13dcca91439a6eda33dc7.jpg",
+  songTitle: "i saw your face",
+  songArtist: "malcolm todd",
+  songArt: "https://i.pinimg.com/736x/13/aa/54/13aa5453cfd087ce194622481aeab948.jpg",
   discordId: "374351552720142336",
   socialUrl: "https://discord.com/users/374351552720142336",
-  badges: {
-    nitro: false,
-    boost: false
-  },
   socials: {
     instagram: "https://instagram.com/throwvoid",
-    twitter:   "https://x.com/sultanwqr",
-    tiktok:    "https://tiktok.com/@0xvoidedd"
+    twitter: "https://x.com/sultanwqr",
+    tiktok: "https://tiktok.com/@0xvoidedd"
   }
 };
