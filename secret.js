b71d2752347823a2c2c134624e186aeea8137b12
SyntaxError <throwvoid1@outlook.com>
2026-09-23 22:59:57 +0300

    Update secret.js

--- secret.js
+++ secret.js
@@ -2,7 +2,7 @@ const CONFIG = {
   songTitle: "i saw your face",
   songArtist: "malcolm todd",
   songArt: "https://i.pinimg.com/736x/13/aa/54/13aa5453cfd087ce194622481aeab948.jpg",
-  discordId: "374351552720142336",
+  discordId: "1254501331130126347",
   socialUrl: "https://discord.com/users/374351552720142336",
   socials: {
     instagram: "https://instagram.com/throwvoid",
